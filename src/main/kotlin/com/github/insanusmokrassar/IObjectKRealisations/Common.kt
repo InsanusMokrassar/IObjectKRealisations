7c631a8427224deb52487aa2be8ba2ca494b1d43
OvsyannikovAlexey <ovsyannikov.alexey95@gmail.com>
2017-09-16 14:21:15 +0800

    rename and replace 'openFile' as extension for file with name readIObject

--- src/main/kotlin/com/github/insanusmokrassar/IObjectKRealisations/Common.kt
+++ src/main/kotlin/com/github/insanusmokrassar/IObjectKRealisations/Common.kt
@@ -2,6 +2,7 @@ package com.github.insanusmokrassar.IObjectKRealisations
 
 import com.github.insanusmokrassar.IObjectK.interfaces.IObject
 import org.json.JSONObject
+import java.io.File
 import java.io.FileInputStream
 
 fun main(args: Array<String>) {
@@ -11,25 +12,4 @@ fun main(args: Array<String>) {
     val jsonIObject : IObject<Any> = JSONIObject(jsonObjectExample);
     println(jsonIObject.toString())
     println(jsonIObject.get<IObject<Any>>("InnerObject").toString())
-}
-
-@Throws(IllegalArgumentException::class)
-fun openFile(filePath: String): IObject<Any> {
-    try {
-        val file = FileInputStream(filePath)
-        var resultException: Exception
-        try {
-            return JSONIObject(file)
-        } catch (e: Exception) {
-            resultException = Exception("File $filePath can't be read as json file: ${e.message}\n${e.stackTrace}", e)
-        }
-        try {
-            return PropertiesIObject(file)
-        } catch (e: Exception) {
-            resultException = Exception("File $filePath can't be read as properties file: ${e.message}\n${e.stackTrace}", resultException)
-        }
-        throw resultException
-    } catch (e: Exception) {
-        throw IllegalArgumentException("For some of reason can't read file", e)
-    }
 }
--- src/main/kotlin/com/github/insanusmokrassar/IObjectKRealisations/Extensions.kt
+++ src/main/kotlin/com/github/insanusmokrassar/IObjectKRealisations/Extensions.kt
@@ -3,6 +3,7 @@ package com.github.insanusmokrassar.IObjectKRealisations
 import com.github.insanusmokrassar.IObjectK.interfaces.IObject
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import java.io.File
 
 private var gson: Gson? = null
 
@@ -12,6 +13,27 @@ fun Any.toIObject(): IObject<Any> {
     } catch (e: NullPointerException) {
         initGSON()
         toIObject()
+    }
+}
+
+@Throws(IllegalArgumentException::class)
+fun File.readIObject(): IObject<Any> {
+    try {
+        val file = inputStream()
+        var resultException: Exception
+        try {
+            return JSONIObject(file)
+        } catch (e: Exception) {
+            resultException = Exception("File $absolutePath can't be read as json file: ${e.message}\n${e.stackTrace}", e)
+        }
+        try {
+            return PropertiesIObject(file)
+        } catch (e: Exception) {
+            resultException = Exception("File $absolutePath can't be read as properties file: ${e.message}\n${e.stackTrace}", resultException)
+        }
+        throw resultException
+    } catch (e: Exception) {
+        throw IllegalArgumentException("For some of reason can't read file", e)
     }
 }
 
